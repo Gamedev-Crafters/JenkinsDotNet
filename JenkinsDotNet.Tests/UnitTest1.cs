7561438198690a17f3a7f11a7bdff2a6ad8a0e94
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-07-20 11:25:39 +0200

    test: fix test

--- JenkinsDotNet.Tests/UnitTest1.cs
+++ JenkinsDotNet.Tests/UnitTest1.cs
@@ -1,11 +1,10 @@
 namespace JenkinsDotNet.Tests;
-// --no-build falla?
 
 public class Tests
 {
     [Test]
     public void PassingTest()
     {
-        Assert.Fail();
+        Assert.Pass();
     }
 }
